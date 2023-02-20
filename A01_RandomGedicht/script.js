2ecafa21c5f68113b9b48b2e9aef4790e7d77637
Leomyn <101626054+Leomyn@users.noreply.github.com>
2023-02-20 10:10:20 +0100

    ein sinnvoller commit

--- A01_RandomGedicht/script.js
+++ A01_RandomGedicht/script.js
@@ -1,26 +1,34 @@
-let subjects = ["Harry", "Hermine", "Ron", "Snape", "Trump", "Dumbledore"];
-let verbs = ["studiert", "hasst", "zaubert", "zerstört", "annektiert", "liebt"];
-let objects = ["Zaubertränke", "Lupin", "Hogwarts", "Dementoren", "Muggel", "den Grimm"];
-console.log(subjects);
-console.log(verbs);
-console.log(objects);
-function getVerse(arr1, arr2, arr3) {
-    for (let i = arr1.length; i >= 1; i--) {
-        let verse = "";
-        let nr = Math.random() * arr1.length;
-        nr = Math.floor(nr);
-        let word = arr1.splice(nr, 1);
-        verse = word[0] + " ";
-        nr = Math.random() * arr1.length;
-        nr = Math.floor(nr);
-        word = arr2.splice(nr, 1);
-        verse += word[0] + " ";
-        nr = Math.random() * arr1.length;
-        nr = Math.floor(nr);
-        word = arr3.splice(nr, 1);
-        verse += word[0] + " ";
-        console.log(verse);
+/*
+Aufgabe: L01_Zufallsgedicht
+Name: Daniel Kelesov
+Matrikel: 268399
+Datum: 20.02.2023
+Quellen: -
+*/
+var Gedicht;
+(function (Gedicht) {
+    let subjects = ["Harry", "Hermine", "Ron", "Snape", "Trump", "Dumbledore"];
+    let verbs = ["studiert", "hasst", "zaubert", "zerstört", "annektiert", "liebt"];
+    let objects = ["Zaubertränke", "Lupin", "Hogwarts", "Dementoren", "Putin", "den Grimm"];
+    //console.log(subjects);
+    //console.log(verbs);
+    //console.log(objects);
+    function getVerse(_arr1, _arr2, _arr3) {
+        for (let i = _arr1.length; i >= 1; i--) {
+            let verse = "";
+            let nr = Math.random() * _arr1.length;
+            nr = Math.floor(nr);
+            let word = _arr1.splice(nr, 1);
+            verse = word[0] + " ";
+            nr = Math.floor(nr);
+            word = _arr2.splice(nr, 1);
+            verse += word[0] + " ";
+            nr = Math.floor(nr);
+            word = _arr3.splice(nr, 1);
+            verse += word[0] + " ";
+            console.log(verse);
+        }
     }
-}
-getVerse(subjects, verbs, objects);
+    getVerse(subjects, verbs, objects);
+})(Gedicht || (Gedicht = {}));
 //# sourceMappingURL=script.js.map
--- A01_RandomGedicht/script.ts
+++ A01_RandomGedicht/script.ts
@@ -1,24 +1,36 @@
-let subjects: string[] = ["Harry","Hermine","Ron","Snape","Trump","Dumbledore"];
-let verbs: string[] = ["studiert","hasst","zaubert","zerstört","annektiert","liebt"];
-let objects: string[] = ["Zaubertränke","Lupin","Hogwarts","Dementoren","Putin","den Grimm"];
-console.log(subjects);
-console.log(verbs);
-console.log(objects);
+/*
+Aufgabe: L01_Zufallsgedicht
+Name: Daniel Kelesov
+Matrikel: 268399
+Datum: 20.02.2023
+Quellen: -
+*/
 
-function getVerse(arr1:string[],arr2:string[],arr3:string[]){
-    for(let i = arr1.length; i >= 1; i--){
-        let verse:string = "";
-        let nr = Math.random()*arr1.length;
-        nr = Math.floor(nr);
-        let word = arr1.splice(nr,1);
-        verse = word[0] + " ";
-        nr = Math.floor(nr);
-        word = arr2.splice(nr,1);
-        verse += word[0] + " ";
-        nr = Math.floor(nr);
-        word = arr3.splice(nr,1);
-        verse += word[0] + " ";
-        console.log(verse);
+
+namespace Gedicht {
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Snape", "Trump", "Dumbledore"];
+    let verbs: string[] = ["studiert", "hasst", "zaubert", "zerstört", "annektiert", "liebt"];
+    let objects: string[] = ["Zaubertränke", "Lupin", "Hogwarts", "Dementoren", "Putin", "den Grimm"];
+    //console.log(subjects);
+    //console.log(verbs);
+    //console.log(objects);
+
+    function getVerse(_arr1: string[], _arr2: string[], _arr3: string[]) {
+        for (let i = _arr1.length; i >= 1; i--) {
+            let verse: string = "";
+            let nr = Math.random() * _arr1.length;
+            nr = Math.floor(nr);
+            let word = _arr1.splice(nr, 1);
+            verse = word[0] + " ";
+            nr = Math.floor(nr);
+            word = _arr2.splice(nr, 1);
+            verse += word[0] + " ";
+            nr = Math.floor(nr);
+            word = _arr3.splice(nr, 1);
+            verse += word[0] + " ";
+            console.log(verse);
+        }
     }
+    getVerse(subjects,verbs,objects);
 }
-getVerse(subjects,verbs,objects);
+
