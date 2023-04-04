ea8ab34bd9f3fa698c512b3cdfd7c391b9eb39dd
Leomyn <101626054+Leomyn@users.noreply.github.com>
2023-04-04 14:57:40 +0200

    added script to A03

--- A03_Aufgabenliste/script.js
+++ A03_Aufgabenliste/script.js
@@ -4,6 +4,26 @@ var Aufgabenliste;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("loaded");
+        let sendBtn = document.querySelector("#sendBtn");
+        sendBtn.addEventListener("click", sendTask);
+        let commentBtn = document.querySelector("#commentBtn");
+        commentBtn.addEventListener("click", commentTask);
+        let progressBtn = document.querySelector("#progressBtn");
+        progressBtn.addEventListener("click", progressTask);
+        let deleteBtn = document.querySelector("#deleteBtn");
+        deleteBtn.addEventListener("click", deleteTask);
+    }
+    function sendTask(_event) {
+        console.log("task sent");
+    }
+    function commentTask(_event) {
+        console.log("task commented");
+    }
+    function progressTask(_event) {
+        console.log("Set Task to in Progress");
+    }
+    function deleteTask(_event) {
+        console.log("task done");
     }
 })(Aufgabenliste || (Aufgabenliste = {}));
 //# sourceMappingURL=script.js.map
--- A03_Aufgabenliste/script.ts
+++ A03_Aufgabenliste/script.ts
@@ -5,5 +5,32 @@ namespace Aufgabenliste{
 
     function handleLoad(_event: Event){
         console.log("loaded");
+        let sendBtn:HTMLElement = <HTMLElement> document.querySelector("#sendBtn");
+        sendBtn.addEventListener("click",sendTask);
+        let commentBtn:HTMLElement = <HTMLElement> document.querySelector("#commentBtn");
+        commentBtn.addEventListener("click",commentTask);
+        let progressBtn:HTMLElement = <HTMLElement> document.querySelector("#progressBtn");
+        progressBtn.addEventListener("click",progressTask);
+        let deleteBtn:HTMLElement = <HTMLElement> document.querySelector("#deleteBtn");
+        deleteBtn.addEventListener("click",deleteTask);
     }
+
+
+
+    function sendTask(_event: Event): void{
+        console.log("task sent");
+    }
+
+    function commentTask(_event: Event): void{
+        console.log("task commented");
+    }
+
+    function progressTask(_event: Event): void{
+        console.log("Set Task to in Progress");
+    }
+    
+    function deleteTask(_event: Event): void{
+        console.log("task done");
+    }
+
 }
